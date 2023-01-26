16ad4d0e71705ee6b28b2da7329f84688a546782
Aydin <habibi.idin@gmail.com>
2023-01-26 12:21:23 +0330

    create QQ4

--- 4.js
+++ 4.js
@@ -1,11 +1,12 @@
 const notification = document.querySelector(".notification");
-function showNotification(top, right, html, className) {
-  notification.style.top = `${top} rem`;
-  notification.style.right = `${right}rem`;
+function showNotification(top, left, html, className) {
+  notification.style.position = "relative";
+  notification.style.top = `${top} px`;
+  notification.style.left = `${left}px`;
   notification.innerHTML = `${html}`;
   notification.classList.add(`${className}`);
 }
-showNotification(10, 20, "Hello", "welecome");
+showNotification(150, 200, "Hello", "welecome");
 // showNotification({
 //   top: 122,
 //   right: 133,
